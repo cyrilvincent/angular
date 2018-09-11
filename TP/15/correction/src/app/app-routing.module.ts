b4d5e3f97bf0b632481c212a9d49b18096e38a30
cyrilvincent <contact@cyrilvincent.com>
2018-09-11 14:10:19 +0200

    tp15

---
+++ TP/15/correction/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { GamesComponent } from './games/games.component';
+import { GameDetailComponent } from './game-detail/game-detail.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/games', pathMatch: 'full' },
+  { path: 'detail/:id', component: GameDetailComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'games', component: GamesComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
+
