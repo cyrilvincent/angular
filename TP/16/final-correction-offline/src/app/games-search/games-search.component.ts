93969961ffd15b584036c1091a091787f6f7243e
cyrilvincent <contact@cyrilvincent.com>
2018-09-11 22:06:17 +0200

    tp16-2

---
+++ TP/16/final-correction-offline/src/app/games-search/games-search.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+ 
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
+ 
+import { VideoGame } from '../video-game';
+import { GameService } from '../game.service';
+
+@Component({
+  selector: 'app-games-search',
+  templateUrl: './games-search.component.html',
+  styleUrls: ['./games-search.component.css']
+})
+export class GamesSearchComponent implements OnInit {
+
+  games$: Observable<VideoGame[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private gameService: GameService) {}
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.games$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.gameService.searchGames(term)),
+    );
+  }
+
+}
---
+++ TP/16/final-correction-offline/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  version: '0.1',
+  copyright: 'Cyril Vincent',
+};
