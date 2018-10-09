60a8d9780086a6437dc2146a02b5ac4c4008c7ae
cyrilvincent <contact@cyrilvincent.com>
2018-10-09 14:40:52 +0200

    Aleysia 6

--- TP/RVG-aleysia/src/app/app.module.ts
+++ TP/RVG-aleysia/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
