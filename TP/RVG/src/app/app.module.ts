bcf6aba0d402c4877ea7aed4e13cfbfd8d0940aa
cyrilvincent <contact@cyrilvincent.com>
2019-11-08 11:47:05 +0100

    Kercia 2.5/3

--- TP/RVG/src/app/app.module.ts
+++ TP/RVG/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from './app.component';
 import { GamesComponent } from './games/games.component';
 import { GameDetailComponent } from './game-detail/game-detail.component';
 import { CartComponent } from './cart/cart.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,12 +17,15 @@ import { CartComponent } from './cart/cart.component';
     GamesComponent,
     GameDetailComponent,
     CartComponent,
+    MessagesComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MaterialModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TP/RVG/src/app/shared/game.service.ts
+++ TP/RVG/src/app/shared/game.service.ts
@@ -1,18 +1,49 @@
 import { Injectable } from '@angular/core';
 import {VIDEOGAMES} from './mock-video-games';
 import {VideoGame, Cart} from './video-game';
+import {Observable, of, from, Subject} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  cart: Cart = new Cart();
+  private _cart: Cart = new Cart();
+  emitChangeSource = new Subject<Cart>();
+  changeEmitted$ = this.emitChangeSource.asObservable();
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getGames(): VideoGame[] {
-    return VIDEOGAMES;
+  get cart$(): Observable<Cart> {
+    return of(this._cart);
   }
 
+  getGames(): Observable<VideoGame[]> {
+    this.messageService.add('GameService: fetched games');
+    return of(VIDEOGAMES.sort((x, y) => x.id - y.id));
+  }
+
+  getGame(id: number): Observable<VideoGame> {
+      this.messageService.add(`GameService: fetched hero id=${id}`);
+      return of(VIDEOGAMES.find(game => game.id === id));
+  }
+
+  getTop4(): Observable<VideoGame[]> {
+    this.messageService.add('GameService: fetched TOP4 games');
+    return of(VIDEOGAMES.sort((x, y) => y.nbView - x.nbView).slice(0, 4));
+  }
+
+  incrementNbView(game: VideoGame): void {
+    game.nbView++;
+    console.log(`Game.nbView++ ${game.id}`);
+    console.log(VIDEOGAMES);
+  }
+
+  addToCart(game: VideoGame): void {
+    this.messageService.add('GameService: add ' + game.title + ' to cart');
+    this._cart.add(game);
+    this.emitChangeSource.next(this._cart);
+  }
 }
