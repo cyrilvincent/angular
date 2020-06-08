6daae663aa20787b90a59f865a51460ea437478e
cyrilvincent <contact@cyrilvincent.com>
2020-06-08 12:12:30 +0200

    Adenes 0.5/3

--- TP/RVG/src/app/app.component.ts
+++ TP/RVG/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RVG';
+  public title = 'Retro VideoGame';
+  public angular = 9;
 }
