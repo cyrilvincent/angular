27150fbcbae0c05cafadb29300c2750b64e7b392
cyrilvincent <contact@cyrilvincent.com>
2020-07-09 14:46:29 +0200

    Adenes 12

--- TP/12/correction/app/games/games.component.spec.ts
+++ TP/12/correction/app/games/games.component.spec.ts
@@ -26,10 +26,5 @@ describe('GamesComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(GamesComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h2').textContent).toContain('Video Game');
-  }));
+
 });
--- TP/RVG/src/app/app.component.spec.ts
+++ TP/RVG/src/app/app.component.spec.ts
@@ -28,7 +28,7 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'RGV'`, async(() => {
+  it(`should have as title 'RVG'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Retro VideoGame');
